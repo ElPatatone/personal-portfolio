08662cbb9203cef211c3185439bd18b9a1faaa07
ElPatatone <rahman.hm2002@gmail.com>
2022-09-10 01:51:32 +0100

    feat: add new design for site

---
+++ circle1.js
@@ -0,0 +1,13 @@
+var message = ["hello.\ni love potatoes.\ni also love machine learning"]
+
+var textPosition = 0;
+var speed = 130;
+
+typewriter = () => {
+  document.getElementById("demo").innerHTML = message[0].substring(0, textPosition) + 
+  "<span class='blinker'></span>";
+  if(textPosition++ != message[0].length)
+    setTimeout(typewriter, speed);
+}
+
+window.addEventListener("load", typewriter);
